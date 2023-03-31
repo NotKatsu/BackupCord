ec6dc811e9c4ca57d7a2894f1bf47bdf97bb7b80
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-31 19:36:07 +0100

    space added and add_user function used

--- helpers/friends.py
+++ helpers/friends.py
@@ -1,12 +1,12 @@
 import httpx 
 
-from helpers.files import create_dir, create_file
+from helpers.files import create_dir, create_file, add_user
 
 httpx_client: object = httpx.Client()
 base_url: str = "https://discord.com/api/v9"
 
 def get_friends(authorization_token: str) -> bool:
-    create_dir("backup");create_file("friends")
+    create_dir("backup"); create_file("friends")
     
     headers = {
         "Authorization": authorization_token, 
@@ -21,8 +21,8 @@ def get_friends(authorization_token: str) -> bool:
                 user_username: str = user["user"]["username"]
                 user_discriminator: int = user["user"]["discriminator"]
                 
-                print(user_id, user_username, user_discriminator)
-                
+                add_user("backup", "friends", user_username, user_discriminator, user_id)
+                return True
             else:
                 pass
     else:
