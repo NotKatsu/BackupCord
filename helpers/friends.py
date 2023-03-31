4b1b18aed8a4cfdb542c581d176cc51c9001b7f9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-31 19:24:57 +0100

    Request for friends created

--- helpers/friends.py
+++ helpers/friends.py
@@ -1,3 +1,25 @@
 import httpx 
 
 httpx_client: object = httpx.Client()
+base_url: str = "https://discord.com/api/v9"
+
+def get_friends(authorization_token: str) -> bool:
+    headers = {
+        "Authorization": authorization_token, 
+        "Content-Type": "application/json"
+    }
+    request: object = httpx_client.get(f"{base_url}/users/@me/relationships", headers=headers)
+    
+    if request.status_code == 200:
+        for user in request.json():
+            if str(user["type"]) == "1":
+                user_id: int = user["user"]["id"]
+                user_username: str = user["user"]["username"]
+                user_discriminator: int = user["user"]["discriminator"]
+                
+                print(user_id, user_username, user_discriminator)
+                
+            else:
+                pass
+    else:
+        return False
